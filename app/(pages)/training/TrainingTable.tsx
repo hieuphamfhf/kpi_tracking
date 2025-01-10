97ae01a78091916bcb46cb086d955a35bc1d464a
hieuphamfhf <henrypham1420@gmail.com>
2025-01-10 13:24:37 +0700

    20250110_done_capNhatGiaTriCO_training_ByNumber

--- app/(pages)/training/TrainingTable.tsx
+++ app/(pages)/training/TrainingTable.tsx
@@ -219,7 +219,7 @@ export default function TrainingTable({ Training, onStartDate, onEndDate, onDepa
                         <SelectContent>
                             {/* <SelectItem key="all" value="ALL">--所有公司--</SelectItem> */}
                             <SelectItem key="lg" value="LG">LG</SelectItem>
-                            <SelectItem key="OD" value="OD">OD</SelectItem>
+                            <SelectItem key="0D" value="0D">0D</SelectItem>
                             <SelectItem key="LT" value="LT">LT</SelectItem>
                         </SelectContent>
                     </Select>
